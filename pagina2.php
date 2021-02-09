f37a2fc89b37cab7832ad88da7519766e131b732
Esmeralda Enriquez <78413520+kimmurielO@users.noreply.github.com>
2021-02-09 13:25:20 +0100

    Add files via upload

---
+++ pagina2.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>Problema</title>
+</head>
+<body>
+	<?php
+		$conexion=mysqli_connect("localhost","root","")
+		or die("Problemas en la conexion");
+		mysqli_select_db($conexion,"phpfacil") or
+		die("Problemas en la seleccion de la base de datos");
+		mysqli_query($conexion, "insert into alumnos(nombre,mail,codigocurso) values
+		('$_REQUEST[nombre]','$_REQUEST[mail]',$_REQUEST[codigocurso])") or die("Problemas en el select".mysqli_error());
+
+		mysqli_close($conexion);
+		echo "El alumno fue dado de alta.";
+	?>
+</body>
+</html>
