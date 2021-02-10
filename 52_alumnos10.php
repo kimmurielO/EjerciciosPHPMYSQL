b2b51436f7a9aebae801d97c22de4fde6b22bd50
Esmeralda Enriquez <78413520+kimmurielO@users.noreply.github.com>
2021-02-10 09:53:06 +0100

    Add files via upload

---
+++ 52_alumnos10.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>Problema</title>
+</head>
+<body>
+	<form action="pagina8.php" method="post">
+	Ingrese el mail del alumno:
+		<input type="text" name="mail"><br>
+		<input type="submit" value="buscar">
+	</form>
+</body>
+</html>
