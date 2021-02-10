be98aae9ca411915e25c770c5beb33b695b842db
Esmeralda Enriquez <78413520+kimmurielO@users.noreply.github.com>
2021-02-10 13:02:52 +0100

    Add files via upload

---
+++ 62_redireccion.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>Problema</title>
+</head>
+<body>
+	<form action="pagina20.php" method="post">
+		Ingrese dirección de sitio web (ej www.google.com):
+		<input type="text" name="direccion" size="30"><br>
+		<input type="submit" value="Redireccionar">
+	</form>
+</body>
+</html>
